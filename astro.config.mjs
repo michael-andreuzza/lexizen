2c0e33f76695efd85b916d4f830cfaed2e18b1c2
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-06-22 21:07:00 +0300

    testing zen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from '@astrojs/tailwind';
-
-import vercel from '@astrojs/vercel';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  site: 'https://example.com',
-  integrations: [ mdx(), sitemap(), tailwind() ],
-  adapter: vercel(),
+  
+  output: "server",
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
 });
